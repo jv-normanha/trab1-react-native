9f9e090e6bb86c4ea4b1060bd8cc93c1b509e215
João Normanha <normanhaspk@gmail.com>
2023-04-10 18:46:53 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require('./img/heart.png')} 
+       style={{width: 60, height: 60}} />
+      <Text  style={{fontSize: 40}}>Health Saúde</Text>
+
+      <TextInput 
+        style={styles.input}
+        placeholder="Nome Completo"
+        keyboardType="default">
+      </TextInput>
+
+      <TextInput 
+        style={styles.input}
+        placeholder="Insira seu email"
+        keyboardType="email-address">
+      </TextInput>
+      
+      <TextInput 
+        style={styles.input}
+        placeholder="Telefone"
+        keyboardType="phone-pad">
+      </TextInput>
+
+      <TouchableOpacity style={styles.button}>
+        <Text>Salvar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +36,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#979797',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    alignItems: 'flex-end',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    width:200,
+    borderRadius:10
+  },
+  input: {
+    width:200,
+    height: 40,
+    alignItems: 'flex-start',
+    margin: 12,
+    backgroundColor: '#FFF',
+    borderWidth: 1,
+    padding: 10,
   },
 });
